06471804fe96eaf524e6a618bf21beda12ac8159
Adewale Orotayo <didymus7007@gmail.com>
2023-08-14 20:43:28 -0700

    refactor

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,18 +2,16 @@
 /**
  * main - main block
  * Description: Print all numbers of base 10, starting from 0.
- * You are not allowed to use any variable of type char.
- * You can only use `putchar` to print to console.
- * You can only use `putchar` twice.
  * Return: 0
  */
 int main(void)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < 10; i++)
+	while (i < 10)
 	{
 		putchar(i + '0');
+		i++;
 	}
 	putchar('\n');
 
