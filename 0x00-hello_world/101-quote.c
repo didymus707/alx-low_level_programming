a0854d3335f9b9f538affb3f70ee3d7337c252aa
Adewale Orotayo <didymus7007@gmail.com>
2023-08-14 18:29:13 -0700

    used write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - function
+ * Return: 1
+ */
+int main(void)
+{
+	write(2,
+	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+	      strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n"));
+	return (1);
+}
